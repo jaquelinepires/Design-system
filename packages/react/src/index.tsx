dd3838163c5c3ff78762762f75a4390229bb0604
Jaqueline <piresjaqueline17@gmail.com>
2022-10-13 15:49:21 -0300

    feat: Creating app in Storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,12 @@
-import { colors } from '@ui/tokens'
+import { styled } from '@stitches/react'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  padding: '$4',
+})
 
 export function App() {
-  return <h1 style={{ color: colors.green300 }}>Hello World</h1>
+  return <Button>Hello World</Button>
 }
