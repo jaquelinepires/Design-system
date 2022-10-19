465091f17950e0adaeb288d89c0fd58d2eeb16aa
Jaqueline <piresjaqueline17@gmail.com>
2022-10-18 22:28:49 -0300

    perf: Component controls

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/jaquelinepires.png',
     alt: 'Jaqueline Pires',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
